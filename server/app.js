179594233f1f205d09703e7e29280e993297efec
zhvirblis <11149023+zhvirblis@users.noreply.github.com>
2020-06-30 15:34:12 +0300

    update packages

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ const cors = require('cors');
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
